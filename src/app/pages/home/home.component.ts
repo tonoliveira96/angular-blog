4268f1cb97c4ab857ebe29b7000ed8e884fc3e13
Everton Oliveira <tonn.oliveira2011@gmail.com>
2024-10-28 20:06:18 -0300

    feat: Estrutura da página 'Home'

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { CardBigComponent } from '../../components/card-big/card-big.component';
+import { CardSmallComponent } from '../../components/card-small/card-small.component';
+import { MenuBarComponent } from '../../components/menu-bar/menu-bar.component';
+import { MenuTitleComponent } from '../../components/menu-title/menu-title.component';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [MenuTitleComponent, CardBigComponent, CardSmallComponent, MenuBarComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
