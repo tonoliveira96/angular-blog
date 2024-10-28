a535d415db4bb59031ff4500341d977abad7699b
Everton Oliveira <tonn.oliveira2011@gmail.com>
2024-10-28 08:28:09 -0300

    feat: Estrutura do projeto

---
+++ src/app/components/card-big/card-big.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-big',
+  standalone: true,
+  imports: [],
+  templateUrl: './card-big.component.html',
+  styleUrl: './card-big.component.css'
+})
+export class CardBigComponent {
+
+}
---
+++ src/app/components/card-small/card-small.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-small',
+  standalone: true,
+  imports: [],
+  templateUrl: './card-small.component.html',
+  styleUrl: './card-small.component.css'
+})
+export class CardSmallComponent {
+
+}
